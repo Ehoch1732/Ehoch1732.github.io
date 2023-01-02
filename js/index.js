f1c795b46d0a81f97db187498aac68d3634c82fd
ehoch <ehoch@umich.edu>
2023-01-02 17:59:46 -0500

    Planning section styling and added respective planning images to project pages

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const subNavLinks = document.querySelectorAll('.subnav__link');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
-});
+})
 
 navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+subNavLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
